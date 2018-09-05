7eecda24cb00d434facbfc90a4508c3c0d303f94
Sosodope <sonadolkar93@hotmail.com>
2018-09-05 23:36:04 +0700

    fetching local data

--- src/app/layout/Training.js
+++ src/app/layout/Training.js
@@ -1,28 +1,52 @@
 import React, { Component } from "react";
-import { posts } from "./training-data.js";
-import { authors } from "./training-data.js";
-
-const postings = { posts };
 
 class Training extends Component {
   state = {
-    posts: postings,
+    todos: [],
     currentPage: 1,
     todosPerPage: 3
   };
 
-  handleClick(event) {
+  handleClick = event => {
     this.setState({
       currentPage: Number(event.target.id)
     });
+  };
+  componentDidMount() {
+    fetch("http://localhost:3000/training-data.json").then(data =>
+      console.log(data)
+    );
   }
-
   render() {
+    const { todos, currentPage, todosPerPage } = this.state;
+
+    // Logic for displaying current todos
+    const indexOfLastTodo = currentPage * todosPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+    const renderTodos = currentTodos.map((todo, index) => {
+      return <li key={index}>{todo}</li>;
+    });
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li key={number} id={number} onClick={this.handleClick}>
+          {number}
+        </li>
+      );
+    });
+
     return (
-      <div>
-        {this.state.posts.map(post => {
-          return <li key={post.id}>{post.body}</li>;
-        })}
+      <div className="container">
+        <ul>{renderTodos}</ul>
+        <ul id="page-numbers">{renderPageNumbers}</ul>
       </div>
     );
   }
